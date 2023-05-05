72e9bcf3b29881dc46c9cfc811cbf3a3b19ac588
Alexandr Alexandrov <alxcube@gmail.com>
2023-05-05 22:35:10 +0300

    Added version. Integrated eslint and prettier

--- src/loadImage.ts
+++ src/loadImage.ts
@@ -1,22 +1,23 @@
+export * from "./version";
+
 /**
  * loadImage() function options.
  */
 export interface LoadImageOptions {
+  /**
+   * Timeout to abort loading in milliseconds.
+   */
+  timeout?: number;
 
-    /**
-     * Timeout to abort loading in milliseconds.
-     */
-    timeout?: number;
+  /**
+   * Cross-Origin value.
+   */
+  crossOrigin?: string | "anonymous" | "use-credentials";
 
-    /**
-     * Cross-Origin value.
-     */
-    crossOrigin?: string | 'anonymous' | 'use-credentials';
-
-    /**
-     * AbortController signal for manual abortion.
-     */
-    signal?: AbortSignal;
+  /**
+   * AbortController signal for manual abortion.
+   */
+  signal?: AbortSignal;
 }
 
 /**
@@ -25,51 +26,54 @@ export interface LoadImageOptions {
  * @param src
  * @param options
  */
-export function loadImage(src: string, options: LoadImageOptions = {}): Promise<HTMLImageElement> {
-    const { timeout = 0, crossOrigin, signal } = options;
+export function loadImage(
+  src: string,
+  options: LoadImageOptions = {}
+): Promise<HTMLImageElement> {
+  const { timeout = 0, crossOrigin, signal } = options;
 
-    return new Promise((resolve, reject) => {
-        const image = new Image();
+  return new Promise((resolve, reject) => {
+    const image = new Image();
 
-        let timer: number;
+    let timer: number;
 
-        function cleanup() {
-            if (timer) {
-                clearTimeout(timer);
-            }
-            image.onerror = null;
-            image.onload = null;
-        }
+    function cleanup() {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      image.onerror = null;
+      image.onload = null;
+    }
 
-        if (timeout > 0) {
-            timer = setTimeout(() => {
-                cleanup();
-                reject(new Error(`Image ${src} timed out after ${timeout}ms.`));
-            }, timeout);
-        }
+    if (timeout > 0) {
+      timer = setTimeout(() => {
+        cleanup();
+        reject(new Error(`Image ${src} timed out after ${timeout}ms.`));
+      }, timeout);
+    }
 
-        image.onerror = () => {
-            cleanup();
-            reject(new Error(`Couldn't load image from ${src}.`));
-        };
+    image.onerror = () => {
+      cleanup();
+      reject(new Error(`Couldn't load image from ${src}.`));
+    };
 
-        image.onload = () => {
-            cleanup();
-            resolve(image);
-        };
+    image.onload = () => {
+      cleanup();
+      resolve(image);
+    };
 
-        if (crossOrigin) {
-            image.crossOrigin = crossOrigin;
-        }
+    if (crossOrigin) {
+      image.crossOrigin = crossOrigin;
+    }
 
-        if (signal) {
-            signal.addEventListener("abort", () => {
-                cleanup();
-                reject(new DOMException("Aborted", "AbortError"));
-                image.src = "";
-            });
-        }
+    if (signal) {
+      signal.addEventListener("abort", () => {
+        cleanup();
+        reject(new DOMException("Aborted", "AbortError"));
+        image.src = "";
+      });
+    }
 
-        image.src = src;
-    });
+    image.src = src;
+  });
 }
